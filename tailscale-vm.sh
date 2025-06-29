cc431fddd2368ef799e7d8e6fff4703bd04f0b0d
Jason <github@harfamily.com>
2025-06-29 16:13:40 -0500

    Update tailscale-vm.sh
    Escaped env variable

--- tailscale-vm.sh
+++ tailscale-vm.sh
@@ -15,7 +15,7 @@ function header_info() {
   / / / __ `/ / / ___/ ___/ __ `/ / _ \    | | / / /|_/ /
  / / / /_/ / / (__  ) /__/ /_/ / /  __/    | |/ / /  / /
 /_/  \__,_/_/_/____/\___/\__,_/_/\___/     |___/_/  /_/
-
+  -JManDoo
 EOF
 }
 header_info
@@ -498,7 +498,7 @@ virt-customize -q -a "${FILE}" --install qemu-guest-agent,apt-transport-https,ca
   virt-customize -q -a "${FILE}" --run-command "mkdir -p /etc/apt/keyrings && curl -fsSL https://pkgs.tailscale.com/stable/debian/bookworm.noarmor.gpg | tee /usr/share/keyrings/tailscale-archive-keyring.gpg" >/dev/null &&
   virt-customize -q -a "${FILE}" --run-command "curl -fsSL https://pkgs.tailscale.com/stable/debian/bookworm.tailscale-keyring.list | tee /etc/apt/sources.list.d/tailscale.list" &&
   virt-customize -q -a "${FILE}" --run-command "apt-get update -qq && apt-get install -y tailscale" >/dev/null &&
-  virt-customize -q -a "${FILE}" --run-command "echo export ts_authkey=$AUTHKEY1" >> /etc/profile.d/custom_env.sh
+  virt-customize -q -a "${FILE}" --run-command "echo export ts_authkey=${AUTHKEY1} >> /etc/profile.d/custom_env.sh" >/dev/null &&
   virt-customize -q -a "${FILE}" --hostname "${HN}" >/dev/null &&
   virt-customize -q -a "${FILE}" --run-command "echo -n > /etc/machine-id" >/dev/null
 msg_ok "Added Tailscale to Debian 12 Qcow2 Disk Image successfully"
@@ -562,4 +562,4 @@ if [ "$START_VM" == "yes" ]; then
   msg_ok "Started Tailscale VM"
 fi
 post_update_to_api "done" "none"
-msg_ok "Completed Successfully!\n Please run: tailscale up --auth-key=$ts_authkey --advertise-exit-node\nIn the newly created Tailscale VM"
+msg_ok "Completed Successfully!\n Please run: tailscale up --auth-key=\$ts_authkey --advertise-exit-node\nIn the newly created Tailscale VM"
